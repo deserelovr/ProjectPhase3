bae909d86e03bad7c9b6dcc7404e5bbb3a9155cf
jasmine.bumbray@my.strayer.edu <jasmine.bumbray@my.strayer.edu>
2023-02-20 05:49:47 -0800

    change date format

--- ProjectPhase3.py
+++ ProjectPhase3.py
@@ -60,7 +60,7 @@ def printinfo(DetailsPrinted):
     # write the line of code that will open the file in read mode and assign it to EmpFile (Hint: see week 6, lab 2 as a guide)
     with open(FILENAME, "r") as EmpFile:
         while True:
-            rundate = input ("Enter start date for report (MM/DD/YYYY) or All for all data in file: ")
+            rundate = input ("Enter start date for report (YYYY/MM/DD) or All for all data in file: ")
             if (rundate.upper() == "ALL"):
                 break
             try:
